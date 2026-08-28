75eb858b680f26faf6c4f760464ba18551775979
JustinEvans <justin.robert.evans@protonmail.com>
2026-08-28 13:31:49 +0100

    fix: correct project ID mapping in Stage 3 secops-rules module JRE 28082026 1331

--- fast/stages/3-secops-dev/main.tf
+++ fast/stages/3-secops-dev/main.tf
@@ -129,7 +129,7 @@ resource "google_apikeys_key" "feed_api_key" {
 
 module "secops-rules" {
   source     = "../../../modules/secops-rules"
-  project_id = var.project_id
+  project_id = module.project.project_id
   tenant_config = {
     region      = var.tenant_config.region
     customer_id = var.tenant_config.customer_id

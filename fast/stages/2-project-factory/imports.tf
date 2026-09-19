e4a74da498d6d1cf56633d4297d71ec25860e057
JustinEvans <justin.robert.evans@protonmail.com>
2026-09-19 22:08:30 +0100

    fix: big query import migration fix import.tf test  JRE 19092026 2206

--- fast/stages/2-project-factory/imports.tf
+++ fast/stages/2-project-factory/imports.tf
@@ -1,4 +1,4 @@
 import {
   to = module.factory.module.bigquery-datasets["ny-prod-project-0/my_dataset"].google_bigquery_dataset.default
-  id = "projects/intrepid-fiber-506711-j5/datasets/my_dataset"
+  id = "projects/placard-ny-prod-project-0/datasets/my_dataset"
 }

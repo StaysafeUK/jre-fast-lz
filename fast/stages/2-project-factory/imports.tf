377e8d4fbb9bc19e06fda29acf0722dde19978f9
JustinEvans <justin.robert.evans@protonmail.com>
2026-09-20 00:16:59 +0100

    fix:removed import.tfas per migration fix JRE 20092026 0016

--- fast/stages/2-project-factory/imports.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = module.factory.module.bigquery-datasets["ny-prod-project-0/my_dataset"].google_bigquery_dataset.default
-  id = "projects/placard-ny-prod-project-0/datasets/my_dataset"
-}
